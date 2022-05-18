fac7677d9eade35c642735cf05289621e021cb18
yossisuper <naoki1023yos@yahoo.co.jp>
2022-05-18 17:55:27 +0900

    fixed Header

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,6 @@ export default function Header() {
         <Link href="/"><li className="nav-item"><a href="" className="nav-link active" aria-current="page">Home</a></li></Link>
         <Link href="/rule/"><li className='nav-item'><a href="#" className="nav-link">Rules</a></li></Link>
         <Link href="/play/"><li className='nav-item'><a href="#" className="nav-link">Play</a></li></Link>
-        <li className="nav-item"><a href="https://github.com/YossiSuper/testsns" className="nav-link">Github</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
       </ul>
     </header>
   );
